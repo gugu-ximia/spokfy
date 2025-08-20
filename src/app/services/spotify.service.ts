cbe9fbacaf176d02fdf2dc128b1fcec4b282b462
gustavo <schimit.gustavo.silva@gmail.com>
2025-08-19 22:29:23 -0300

    agro é show

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -8,6 +8,20 @@ import { SpotifyConfiguration } from "../../enviroment/enviroment";
 export class SpotifyService {
     obterUrlLogin(): Promise<string> {
         const authPoint = `${SpotifyConfiguration.authEndpoint}?`;
+        const clientId = `client_id=${SpotifyConfiguration.clientId}&`;
+        const urlRedirect = `redirect_url=${SpotifyConfiguration.redirectUrl}&`;
+        const scopes = `scope=${SpotifyConfiguration.scopes.join('%20')}`;
+        const codeChallengeMethod = 'code_challend_method=s256';
+        const codeChallengeParam = 'code_challend' + codigoAleatorio + '&';
+        const responseType = 'response_type=code';
+        return `${authPoint}${clientId}${urlRedirect}${scopes}${codeChallengeMethod}${codeChallengeParam}${responseType};`
+    }
+
+    async gerarCodigoAleatorio() {
+        const codigoVerificador = this.gerarCodigoVerificador;
+        const codChallenge = await this.gararHash(codigoVerificador)
+
+        return ''; codChallenge;
     }
     constructor(){}
 }
